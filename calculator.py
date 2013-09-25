9d5a6b1b1483b1db7bc28f70c17ec318d4db2b62
Hackbright Student <user@chromebox07.(none)>
2013-09-25 16:38:35 -0700

    takes more than 2 integers as input

--- calculator.py
+++ calculator.py
@@ -12,18 +12,20 @@ operator_dict = {
 }
 
 
-def check_input(tokens, token_len):
+def check_number_of_tokens(tokens, token_len):
     if len(tokens) != token_len:
         print "Wrong number of tokens. Try again."
         return False
-    else:
-        for i in range(1, token_len):
-            token = tokens[i]
-            for char in token:
-                if char not in "0123456789":
-                    print "Please enter an operator followed by integer(s)."
-                    return False
-        return True
+    return True
+
+def check_integers(tokens):
+    for i in range(1, len(tokens)):
+        token = tokens[i]
+        for char in token:
+            if char not in "0123456789":
+                print "Please enter an operator followed by integer(s)."
+                return False
+    return True
 
 def main():
     while True:
@@ -33,22 +35,38 @@ def main():
 
         if token[0] == "q":
             exit(0)
-        elif token[0] in ("+", "-", "*", "/", "pow", "mod"):
-            if check_input(token, 3):
+        elif token[0] in ("+", "-", "*", "/"):
+            if check_integers(token):
+                function_to_run = operator_dict[token[0]] 
+
+                # this should be the first number
+                result = token[1]
+
+                #starting at 2 since result is index 1. this lets us input more than just 2 integers, ex. + 2 2 2 
+                for i in range(2, len(token)):
+                    if token[0] == "/":
+                        result = float(result)
+                        result = function_to_run(result, float(token[i]))
+                        
+                    else:
+                        result = int(result)
+                        result = function_to_run(result, int(token[i]))
+                
+                print result
+
+        elif token[0] in ("pow", "mod"):
+            if check_number_of_tokens(token, 3):
                 # try to convert strings to ints or floats if dividing
-
+                if check_integers(token):
                 # then run appropriate function
-                function_to_run = operator_dict[token[0]] #this will be a function in arithmetic
-
-                if token[0] == "/":
-                    print function_to_run(float(token[1]), float(token[2]))
-                else:
+                    function_to_run = operator_dict[token[0]] #this will be a function in arithmetic
                     print function_to_run(int(token[1]), int(token[2]))
 
         elif token[0] in ("square", "cube"):
-            if check_input(token, 2):
-                function_to_run = operator_dict[token[0]] #this will be a function in arithmetic
-                print function_to_run(int(token[1]))
+            if check_number_of_tokens(token, 2):
+                if check_integers(token):
+                    function_to_run = operator_dict[token[0]] #this will be a function in arithmetic
+                    print function_to_run(int(token[1]))
         else:
             print "Something is wrong"
 
